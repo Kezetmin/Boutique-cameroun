790be38054420a776147f72a8453424fe45fd232
Kezetmin <yvesketz96@gmail.com>
2026-05-03 12:47:16 +0100

    update3

--- config/settings.py
+++ config/settings.py
@@ -149,31 +149,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://frontend-gestion-boutique.vercel.app",
-    "https://frontend-gestion-boutique-git-main-kezetmins-projects.vercel.app",
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://frontend-gestion-boutique.vercel.app",
-    "https://frontend-gestion-boutique-git-main-kezetmins-projects.vercel.app",
-]
 
-CORS_ALLOW_HEADERS = [
-    "accept",
-    "authorization",
-    "content-type",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
-]
-
-CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
-]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
